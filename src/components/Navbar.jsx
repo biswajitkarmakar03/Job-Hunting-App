0e2c11236f8af8826a6fadf9cf5dbfe2ddecf499
biswajitkarmakar03 <biswajitkarmakar404@gmail.com>
2025-07-09 14:21:27 +0530

    Corrected the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,28 @@
 import { Link } from 'react-router-dom';
-import logo  from "../assets/logo.png";
-
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className='bg-indigo-800 border-b border-indigo-700'>
-        <div className="mx-auto max-w-7xt px-2 sm:px-6 lg:px-8">
-            <div className="flex h-20 item-center justify-between">
-                <div className="flex flex-1 item center justify-center md:item-stretch md:justify-start">
-                    <Link to="/" className="flex flex-shrink-0 item-center md:items-stretch md:justify-start">
-                        <img  className='h-10 w-auto ' src={logo}/>
-                        <span className="md:block text-white text-3xl ml-2">
-                           Opportuna
-                        </span>
-                    </Link>
-                    <div className="md:ml-auto">
-                        <div className="flex space-x-2">
-                            <Link to='/'>
-                            
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
-  )
-}
+    <nav className="bg-indigo-800 border-b border-indigo-700">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-20 items-center justify-between">
+          {/* Logo and Brand */}
+          <Link to="/" className="flex items-center">
+            <img src={logo} alt="Logo" className="h-10 w-auto" />
+            <span className="text-white text-2xl font-semibold ml-2">Opportuna</span>
+          </Link>
 
-export default Navbar
+          {/* Navigation Links */}
+          <div className="flex space-x-6">
+            <Link to="/" className="text-white hover:text-indigo-300 transition">Home</Link>
+            <Link to="/jobs" className="text-white hover:text-indigo-300 transition">Jobs</Link>
+            <Link to="/companies" className="text-white hover:text-indigo-300 transition">Companies</Link>
+            <Link to="/services" className="text-white hover:text-indigo-300 transition">Services</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
